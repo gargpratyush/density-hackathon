02dc90a5eb2c5a42e619c106f34c2dd3ba956175
gargpratyush <pratyush.garg@kgpian.iitkgp.ac.in>
2022-10-28 23:16:16 +0530

    initialised backend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,27 @@
+import React, { useEffect, useState} from 'react';
+
+
 function App() {
+
+    const [backendData, setBackendData] = useState([{}])
+
+    useEffect(() => {
+        fetch("/api").then(
+            response => response.json()
+        ).then(
+            data => {
+                setBackendData(data)
+            }
+        )
+    }, [])
+
   return (
     <div className="App">
       User Portfolio
-    </div>
+
+      
+
+    </div> 
   );
 }
 
