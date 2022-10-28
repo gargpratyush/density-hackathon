f7a5dde0002f98fc9a1f168682ccbfbfed0f1f6a
chirag-ghosh <cghosh828049@gmail.com>
2022-10-29 05:06:48 +0530

    db connect done;

--- backend/controllers/userC.js
+++ backend/controllers/userC.js
@@ -1,8 +1,8 @@
 const db = require("../database/connection")
-const mysql = require("mysql");
 
 const getUsersData = async (req, res) => {
- res.send('testing getUsersData');
+    const [temp] = await db.execute('SHOW TABLES;', []);
+    res.json(temp);
 }
 
 module.exports = {
