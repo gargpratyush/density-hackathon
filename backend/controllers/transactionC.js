995e23136dd14358a9d421f905c4142a5ffa3206
gargpratyush <pratyush.garg@kgpian.iitkgp.ac.in>
2022-10-29 13:32:18 +0530

    deleted extra route

--- backend/controllers/transactionC.js
+++ backend/controllers/transactionC.js
@@ -1,11 +1,5 @@
 const db = require("../database/connection")
 
-const getMarketPrice = async (req, res) => {
-    let price_query = `SELECT * FROM ${process.env.MYSQLDATABASE}.market_price;`
-    const [prices] = await db.execute(price_query, []);
-    res.json(prices);
-}
-
 const getTransactionHistory = async (req, res) => {
     let transaction_query = `SELECT * FROM ${process.env.MYSQLDATABASE}.transaction_history;`
     const [transactions] = await db.execute(transaction_query, []);
@@ -13,5 +7,5 @@ const getTransactionHistory = async (req, res) => {
 }
 
 module.exports = {
-    getMarketPrice, getTransactionHistory
+    getTransactionHistory
 }
