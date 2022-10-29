a6f853125fca510b7d9d2e15dcd9a0360f98253b
chirag-ghosh <cghosh828049@gmail.com>
2022-10-29 15:23:35 +0530

    Removed errors;

--- front-end/src/components/OrderPage/BuyOrSell.js
+++ front-end/src/components/OrderPage/BuyOrSell.js
@@ -4,14 +4,14 @@ function BuyOrSell() {
   return (
     <div>
       <div className="Buy-OR-Sell">
-      <select class="form-select" aria-label="Default select example">
+      <select className="form-select" aria-label="Default select example">
         <option selected>Buy OR Sell</option>
         <option value="1">Buy</option>
         <option value="2">Sell</option>
     </select>
       </div>
       <div className='Select-User'>
-      <select class="form-select" aria-label="Default select example">
+      <select className="form-select" aria-label="Default select example">
         <option selected>Select a User</option>
         <option value="1">A</option>
         <option value="2">B</option>
@@ -21,15 +21,15 @@ function BuyOrSell() {
     </select>
       </div>
       <div className="Order-Type">
-      <select class="form-select" aria-label="Default select example">
+      <select className="form-select" aria-label="Default select example">
         <option selected>Order Type (Limit / Market)</option>
         <option value="1">Limit</option>
         <option value="2">Market</option>
     </select>
       </div>
       <div className='Stock/Price'>
-      <input class="form-control" type="text" placeholder="Stock Amount" aria-label="default input example"></input>
-      <input class="form-control" type="text" placeholder="Price" aria-label="default input example"></input>
+      <input className="form-control" type="text" placeholder="Stock Amount" aria-label="default input example"></input>
+      <input className="form-control" type="text" placeholder="Price" aria-label="default input example"></input>
       </div>
     </div>
   )
