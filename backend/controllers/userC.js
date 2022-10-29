c6e8cb23fd87ca4ce94d08526d179beb25c34a5d
gargpratyush <pratyush.garg@kgpian.iitkgp.ac.in>
2022-10-29 06:27:55 +0530

    GET /users

--- backend/controllers/userC.js
+++ backend/controllers/userC.js
@@ -1,8 +1,9 @@
 const db = require("../database/connection")
 
 const getUsersData = async (req, res) => {
-    const [temp] = await db.execute('SHOW TABLES;', []);
-    res.json(temp);
+    let user_query = `SELECT * FROM ${process.env.MYSQLDATABASE}.users;`
+    const [users] = await db.execute(user_query, []);
+    res.json(users);
 }
 
 module.exports = {
